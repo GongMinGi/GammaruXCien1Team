5808a8d5c9ef4f5f147fd555bf89b4da4b9ce8b0
GongMinGi <komki@naver.com>
2026-08-20 17:51:54 +0900

    feat: HP UI를 슬라이더 기반으로 교체
    플레이어와 보스 HP 바를 코드로 텍스처를 그려 스케일하던 방식에서
    빈 바/꽉찬 바 스프라이트를 겹친 World Space Canvas 슬라이더로 변경.
    Fill Origin으로 플레이어는 오른쪽부터, 보스는 왼쪽부터 깎이게 한다.
    
    - PlayerStatsDisplay, BossStatsDisplay: 바 생성 코드와 색상 필드 삭제
    - PlayerStatsDisplay, BossStatsDisplay: hpSlider 참조 추가, Refresh는 value 대입만
    - HP 비율에 따른 색 보간 제거, 스프라이트 색을 그대로 사용

--- Assets/Scripts/Battle/Boss/BossStatsDisplay.cs
+++ Assets/Scripts/Battle/Boss/BossStatsDisplay.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossStatsDisplay : MonoBehaviour
 {
     [SerializeField] private BossStats bossStats;
+    [SerializeField] private Slider hpSlider;
 
-    [Header("HP Bar")]
-    [SerializeField] private float barWidth = 4.45f;
-    [SerializeField] private float barHeight = 0.25f;
-    [SerializeField] private Color bgColor = new Color(0.2f, 0.2f, 0.2f);
-
-    private Transform hpFillTransform;
-    private SpriteRenderer hpFillRenderer;
     private TextMesh hpText;
 
     private void Start()
@@ -22,7 +17,7 @@ public class BossStatsDisplay : MonoBehaviour
             return;
         }
 
-        GenerateVisuals();
+        GenerateTexts();
         bossStats.StatsChanged += Refresh;
         bossStats.DamageTaken += ShowDamage;
         Refresh();
@@ -37,33 +32,8 @@ public class BossStatsDisplay : MonoBehaviour
         }
     }
 
-    private void GenerateVisuals()
+    private void GenerateTexts()
     {
-        Sprite barSprite = CreateBarSprite();
-
-        GameObject bgObj = new GameObject("HpBarBg");
-        bgObj.transform.SetParent(transform, false);
-        bgObj.transform.localPosition = Vector3.zero;
-        bgObj.transform.localScale = new Vector3(barWidth, barHeight, 1f);
-
-        SpriteRenderer bgRenderer = bgObj.AddComponent<SpriteRenderer>();
-        bgRenderer.sprite = barSprite;
-        bgRenderer.color = bgColor;
-        bgRenderer.sortingOrder = 1;
-
-        Sprite fillSprite = CreateFillSprite();
-
-        GameObject fillObj = new GameObject("HpBarFill");
-        fillObj.transform.SetParent(transform, false);
-        fillObj.transform.localPosition = new Vector3(-barWidth * 0.5f, 0f, 0f);
-        fillObj.transform.localScale = new Vector3(barWidth, barHeight, 1f);
-
-        hpFillRenderer = fillObj.AddComponent<SpriteRenderer>();
-        hpFillRenderer.sprite = fillSprite;
-        hpFillRenderer.color = Color.red;
-        hpFillRenderer.sortingOrder = 2;
-        hpFillTransform = fillObj.transform;
-
         GameObject hpTextObj = new GameObject("HpText");
         hpTextObj.transform.SetParent(transform, false);
         hpTextObj.transform.localPosition = new Vector3(0f, 0f, -0.01f);
@@ -84,41 +54,12 @@ public class BossStatsDisplay : MonoBehaviour
         float ratio = Mathf.Clamp01(
             (float)bossStats.CurrentHp / Mathf.Max(1, bossStats.MaxHp));
 
-        hpFillTransform.localScale = new Vector3(ratio * barWidth, barHeight, 1f);
-        hpFillRenderer.color = Color.Lerp(Color.red, new Color(0.8f, 0.2f, 0.2f), ratio);
+        hpSlider.value = ratio;
         hpText.text = $"{bossStats.CurrentHp}/{bossStats.MaxHp}";
     }
 
     private void ShowDamage(int amount)
     {
         DamagePopup.Spawn(transform, amount, new Vector3(0f, 0.55f, -0.02f));
-    }
-
-    private Sprite CreateBarSprite()
-    {
-        int size = 4;
-        Texture2D tex = new Texture2D(size, size);
-        tex.filterMode = FilterMode.Point;
-
-        for (int y = 0; y < size; y++)
-            for (int x = 0; x < size; x++)
-                tex.SetPixel(x, y, Color.white);
-
-        tex.Apply();
-        return Sprite.Create(tex, new Rect(0, 0, size, size), new Vector2(0.5f, 0.5f), size);
-    }
-
-    private Sprite CreateFillSprite()
-    {
-        int size = 4;
-        Texture2D tex = new Texture2D(size, size);
-        tex.filterMode = FilterMode.Point;
-
-        for (int y = 0; y < size; y++)
-            for (int x = 0; x < size; x++)
-                tex.SetPixel(x, y, Color.white);
-
-        tex.Apply();
-        return Sprite.Create(tex, new Rect(0, 0, size, size), new Vector2(0f, 0.5f), size);
     }
 }
--- Assets/Scripts/Battle/PlayerStatsDisplay.cs
+++ Assets/Scripts/Battle/PlayerStatsDisplay.cs
@@ -1,17 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStatsDisplay : MonoBehaviour
 {
     [SerializeField] private PlayerStats playerStats;
     [SerializeField] private ActionBar actionBar;
-
-    [Header("HP Bar")]
-    [SerializeField] private float barHeight = 0.25f;
-    [SerializeField] private Color bgColor = new Color(0.2f, 0.2f, 0.2f);
+    [SerializeField] private Slider hpSlider;
 
     private float barWidth;
-    private Transform hpFillTransform;
-    private SpriteRenderer hpFillRenderer;
     private TextMesh hpText;
     private TextMesh spellPowerText;
 
@@ -25,7 +21,7 @@ public class PlayerStatsDisplay : MonoBehaviour
         }
 
         barWidth = actionBar.VisualWidth;
-        GenerateVisuals();
+        GenerateTexts();
         playerStats.StatsChanged += Refresh;
         playerStats.DamageTaken += ShowDamage;
         Refresh();
@@ -40,36 +36,8 @@ public class PlayerStatsDisplay : MonoBehaviour
         }
     }
 
-    private void GenerateVisuals()
+    private void GenerateTexts()
     {
-        Sprite barSprite = CreateBarSprite();
-
-        // Background
-        GameObject bgObj = new GameObject("HpBarBg");
-        bgObj.transform.SetParent(transform, false);
-        bgObj.transform.localPosition = Vector3.zero;
-        bgObj.transform.localScale = new Vector3(barWidth, barHeight, 1f);
-
-        SpriteRenderer bgRenderer = bgObj.AddComponent<SpriteRenderer>();
-        bgRenderer.sprite = barSprite;
-        bgRenderer.color = bgColor;
-        bgRenderer.sortingOrder = 1;
-
-        // Fill (left-aligned pivot)
-        Sprite fillSprite = CreateFillSprite();
-
-        GameObject fillObj = new GameObject("HpBarFill");
-        fillObj.transform.SetParent(transform, false);
-        fillObj.transform.localPosition = new Vector3(-barWidth * 0.5f, 0f, 0f);
-        fillObj.transform.localScale = new Vector3(barWidth, barHeight, 1f);
-
-        hpFillRenderer = fillObj.AddComponent<SpriteRenderer>();
-        hpFillRenderer.sprite = fillSprite;
-        hpFillRenderer.color = Color.green;
-        hpFillRenderer.sortingOrder = 2;
-        hpFillTransform = fillObj.transform;
-
-        // HP Text
         GameObject hpTextObj = new GameObject("HpText");
         hpTextObj.transform.SetParent(transform, false);
         hpTextObj.transform.localPosition = new Vector3(0f, 0f, -0.01f);
@@ -84,7 +52,6 @@ public class PlayerStatsDisplay : MonoBehaviour
         MeshRenderer hpTextRenderer = hpTextObj.GetComponent<MeshRenderer>();
         hpTextRenderer.sortingOrder = 3;
 
-        // Spell Power Text
         GameObject spObj = new GameObject("SpellPowerText");
         spObj.transform.SetParent(transform, false);
         spObj.transform.localPosition = new Vector3(barWidth * 0.5f + 0.5f, 0f, -0.01f);
@@ -105,8 +72,7 @@ public class PlayerStatsDisplay : MonoBehaviour
         float ratio = Mathf.Clamp01(
             (float)playerStats.CurrentHp / Mathf.Max(1, playerStats.MaxHp));
 
-        hpFillTransform.localScale = new Vector3(ratio * barWidth, barHeight, 1f);
-        hpFillRenderer.color = Color.Lerp(Color.red, Color.green, ratio);
+        hpSlider.value = ratio;
         hpText.text = $"{playerStats.CurrentHp}/{playerStats.MaxHp}";
         spellPowerText.text = $"SP {playerStats.SpellPower}";
     }
@@ -114,33 +80,5 @@ public class PlayerStatsDisplay : MonoBehaviour
     private void ShowDamage(int amount)
     {
         DamagePopup.Spawn(transform, amount, new Vector3(0f, 0.55f, -0.02f));
-    }
-
-    private Sprite CreateBarSprite()
-    {
-        int size = 4;
-        Texture2D tex = new Texture2D(size, size);
-        tex.filterMode = FilterMode.Point;
-
-        for (int y = 0; y < size; y++)
-            for (int x = 0; x < size; x++)
-                tex.SetPixel(x, y, Color.white);
-
-        tex.Apply();
-        return Sprite.Create(tex, new Rect(0, 0, size, size), new Vector2(0.5f, 0.5f), size);
-    }
-
-    private Sprite CreateFillSprite()
-    {
-        int size = 4;
-        Texture2D tex = new Texture2D(size, size);
-        tex.filterMode = FilterMode.Point;
-
-        for (int y = 0; y < size; y++)
-            for (int x = 0; x < size; x++)
-                tex.SetPixel(x, y, Color.white);
-
-        tex.Apply();
-        return Sprite.Create(tex, new Rect(0, 0, size, size), new Vector2(0f, 0.5f), size);
     }
 }
